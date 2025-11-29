2fda93d876a52120d3f9a5559a3a1beb1bcf244c
Rafael Melo <rafaelmelo01111998@gmail.com>
2025-11-28 22:15:56 -0300

    refatoracao do jogo

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,15 @@
 import express from 'express';
 
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+
+app.listen(3000, () =>{
+    console.log("server rodando na porta: " + PORT)
+})
+
+app.get('/primeiro', (req, res) =>{
+
+    return res.status(200).json({primeiro: "primeiro get feito sozinho"})
+});
